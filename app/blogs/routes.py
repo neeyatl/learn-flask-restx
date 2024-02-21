d1ff354bdf0e1c8dd2316164333e4863b3c23eb4
neeyatl <neeyat.lotlikar@gmail.com>
2024-02-21 15:39:42 +0530

    enhanced templates

--- app/blogs/routes.py
+++ app/blogs/routes.py
@@ -6,3 +6,8 @@ from app.blogs.models import Comment, Post, Tag
 def index():
     posts = Post.query.all()
     return render_template('blogs/index.html', posts=posts)
+
+@bp.route('/blogs/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('blogs/show_post.html', post=post)
